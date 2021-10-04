85f3c53dae129c25593c50c23fd49f8953a5a2f0
Raphael Vizoni do Prado <ravizoni@gmail.com>
2021-10-04 17:59:38 -0300

    lista atualizando com input

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,61 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const axios = require("axios").default;
+	const baseURL = "https://pokeapi.co/api/v2/";
+
+	const [pokemons, setPokemons] = useState<any[]>([]);
+	const [myPokemon, setMyPokemon] = useState({});
+	const [searchPokemon, setSearchPokemon] = useState("");
+
+	useEffect(() => {
+		async function getPokemons() {
+			const pokemonsFromAPI = await getFirstTenPokemons();
+			setPokemons(pokemonsFromAPI.data.results);
+		}
+		getPokemons();
+	}, []);
+
+	const getFirstTenPokemons = async () => {
+		return await axios.get(baseURL + "pokemon?limit=10");
+	};
+
+	const getPokemon = async (name: string) => {
+		return await axios.get(baseURL + "pokemon/" + name);
+	};
+
+	const updateMyPokemon = async (name: string) => {
+		const poke = await getPokemon(name);
+		setMyPokemon(poke.data);
+	};
+
+	return (
+		<div className="app">
+			<input
+				type="text"
+				value={searchPokemon}
+				className="search-name"
+				onChange={(event) => setSearchPokemon(event.target.value)}
+			/>
+			{pokemons
+				.filter(
+					(poke) => searchPokemon === "" || poke.name.includes(searchPokemon)
+				)
+				.map((poke: any) => {
+					return (
+						<>
+							<div
+								className="list-item"
+								onClick={() => updateMyPokemon(poke.name)}
+							>
+								{poke.name}
+							</div>
+						</>
+					);
+				})}
+		</div>
+	);
 }
 
 export default App;
